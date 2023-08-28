591ec12cd444f3b5e4017a9d4455e0ffa6481395
uk-taniyama <uk.taniyama@gmail.com>
2023-08-29 01:45:00 +0900

    binを公開。

---
+++ bin/cli.cjs
@@ -0,0 +1,10 @@
+const { readFileSync, writeFileSync } = require('fs');
+
+const { transformString } = require('../dist/index.cjs');
+
+const input = process.argv[2] || '/dev/stdin';
+const output = process.argv[3] || '/dev/stdout';
+
+const text = readFileSync(input, 'utf-8');
+const result = transformString(text);
+writeFileSync(output, result, 'utf-8');
